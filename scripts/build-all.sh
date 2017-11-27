84cfdfa5652f68dc3c17bfd1c0b78b690bdb1321
Alejandro Laban <alejandro.laban@gmail.com>
2017-11-27 07:41:01 -0500

    [FEAT] unix scripts

---
+++ scripts/build-all.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+export CURRENT_DIR=${PWD}
+
+export DEV_PATH=$CURRENT_DIR
+
+#LEGACY
+export CORE=digital-channels-core
+export MQ=digital-channels-mq
+export PROXY=digital-channels-proxy
+export DAO=digital-channels-dao
+
+export POM=-pom
+
+#ICAA
+export ICAA=interbank-common-application-architecture
+export ICAA_CORE=icaa-core-parent
+
+#BPI
+export BPI=bpi-web-app
+
+export PARENT=-parent
+
+# BUILDING LEGACY
+echo # BUILDING LEGACY
+
+#CORE
+cd $DEV_PATH/$CORE/$CORE$POM
+mvn clean install -Dmaven.test.skip=true
+
+#MQ
+cd $DEV_PATH/$MQ/$MQ$POM
+mvn clean install -Dmaven.test.skip=true
+
+#CORE
+cd $DEV_PATH/$PROXY/$PROXY$POM
+mvn clean install -Dmaven.test.skip=true
+
+#CORE
+cd $DEV_PATH/$DAO/$DAO$POM
+mvn clean install -Dmaven.test.skip=true
+
+
+# BUILDING ICAA
+echo # BUILDING ICAA
+
+#ICAA
+cd $DEV_PATH/$ICAA/$ICAA_CORE
+mvn clean install -Dmaven.test.skip=true
+
+# BUILDING BPI
+echo # BUILDING BPI
+
+#BPI
+cd $DEV_PATH/$BPI/$BPI$PARENT
+mvn clean install -Dmaven.test.skip=true
