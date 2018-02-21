3bed871765e1e87cc4be20f6bb1558d3dcdaedd5
Alejandro Laban <alejandro.laban@gmail.com>
2018-02-21 07:50:13 -0500

    feat: Add properties for logback and spring profile

--- scripts/@run_wlp.bat
+++ scripts/@run_wlp.bat
@@ -1,6 +1,8 @@
-rem SET JVM_ARGS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=7777
-SET JAVA_HOME=D:\TCS\Tools\jdk1.8.0_144
+SET JVM_ARGS=%JVM_ARGS% -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=7777
+SET JVM_ARGS=%JVM_ARGS% -Dlogback.configurationFile=C:\apps\digital-channels\bpi\api\conf\logback.xml
+REM SET spring.profiles.active=dev
+SET JAVA_HOME=D:\Tools\Java\jdk1.8.0_121
 SET PATH=%JAVA_HOME%\bin;
-cd D:\TCS\Tools\wlp-javaee7-17.0.0.3\wlp\bin
+cd D:\Tools\wlp-javaee7-17.0.0.3\wlp\bin
 server.bat run defaultServer
 pause
